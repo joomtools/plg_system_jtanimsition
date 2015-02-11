bcc035c5cfb130b2fe5418dd6d74f79eef35691d
Guido De Gobbis <guido.de.gobbis@joomtools.de>
2015-02-11 16:02:05 +0100

    Init v1.0.1

---
+++ jtanimsition.php
@@ -0,0 +1,312 @@
+<?php
+/**
+ * @version 1.0.1
+ * @package JT - Animsition
+ * @copyright 2014 Guido De Gobbis - JoomTools
+ * @license GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.joomtools.de
+ */
+
+defined('_JEXEC') or die;
+
+class plgSystemJTAnimsition extends JPlugin
+{
+	protected $FB = false;
+	protected $paramsSet = array();
+
+	public function __construct(& $subject, $config)
+	{
+		if(version_compare(JVERSION, '3.0', 'ge')){
+			// Class overwrites
+			JLoader::register('JDocumentHTML', JPATH_PLUGINS . '/system/jtanimsition/library/html.php');
+		}
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	public function onAfterInitialise()
+	{
+		if(defined('JFIREPHP') && $this->params->get('debug', 0)) {
+			$this->FB = FirePHP::getInstance(TRUE);
+		} else {
+			$this->FB = FALSE;
+		}
+
+		$FB = $this->FB;
+		$globalParams = $itemParams = array();
+
+		if($FB) $FB->group('JT - Animsition -> '.__FUNCTION__);
+		if($FB) $FB->info($this->params,'$this->params');
+
+		$globalParams['in']['active'] = $this->params->get('anim_active_in');
+		$globalParams['in']['type'] = $this->params->get('anim_type_in');
+		$globalParams['in']['weight'] = $this->params->get('anim_weight_in');
+		$globalParams['in']['dur'] = $this->params->get('anim_dur_in');
+		$globalParams['out']['active'] = $this->params->get('anim_active_out');
+		$globalParams['out']['type'] = $this->params->get('anim_type_out');
+		$globalParams['out']['weight'] = $this->params->get('anim_weight_out');
+		$globalParams['out']['dur'] = $this->params->get('anim_dur_out');
+			if($FB) $FB->info($globalParams,'$globalParams');
+
+		$menuItems = &JFactory::getApplication()->getMenu()->getItems('access', '1');
+
+		if($FB) $FB->info($menuItems,'$menuItems');
+
+		foreach($menuItems as &$item)
+		{
+			if($FB) $FB->group('$item['.$item->id.']');
+			if($FB) $FB->info($item,'$item');
+
+			$itemParams['in']['active'] = $item->params->get('menu_anim_active_in', '-1');
+			$itemParams['in']['type'] = $item->params->get('menu_anim_type_in', '-1');
+			$itemParams['in']['weight'] = $item->params->get('menu_anim_weight_in', '-1');
+			$itemParams['in']['dur'] = $item->params->get('menu_anim_dur_in', '-1');
+			$itemParams['out']['active'] = $item->params->get('menu_anim_active_out', '-1');
+			$itemParams['out']['type'] = $item->params->get('menu_anim_type_out', '-1');
+			$itemParams['out']['weight'] = $item->params->get('menu_anim_weight_out', '-1');
+			$itemParams['out']['dur'] = $item->params->get('menu_anim_dur_out', '-1');
+
+			if($FB) $FB->info($itemParams,'$itemParams');
+
+			$setParams = $this->_setParams($globalParams, $itemParams);
+
+			if($setParams['in']['active'] || $setParams['out']['active'])
+			{
+				if($FB) $FB->info('item['.$item->id.']->active');
+
+				$setAnimsition = $this->_setAnimsition($setParams);
+
+				$activLinkClass = $item->params->get('menu-anchor_css', '');
+				$activLinkClass = $activLinkClass ? $activLinkClass.' animsition-link' : 'animsition-link';
+
+				$item->params->set('menu-anchor_css', $activLinkClass);
+
+				if($FB) $FB->info('item['.$item->id.']->class->set->ok');
+
+				$this->paramsSet['item['.$item->id.']'] = $setAnimsition;
+				$this->paramsSet['item['.$item->id.']']['container'] = $this->params->get('anim_container', 1);
+			}
+
+			if($FB) $FB->groupEnd();
+		}
+
+		if($FB) $FB->groupEnd();
+	}
+
+	public function onContentPrepareForm($form, $data)
+	{
+		$FB = $this->FB;
+		$skipMenus = array('alias', 'heading', 'url', 'separator');
+
+		if($FB) $FB->group('JT - Animsition -> '.__FUNCTION__);
+
+		if($FB) $FB->info($form, '$form');
+		if(is_array($data)) {
+			if($FB) $FB->info($data['type'], '$data[type]');
+			$skipMenu = in_array($data['type'], $skipMenus);
+			if($FB) $FB->info($skipMenu, '$skipMenu');
+		} else {
+			if($FB) $FB->info($data, '$data');
+		}
+		if($FB) $FB->info($form->getName(), '$form->getName()');
+
+		if ($form->getName() == 'com_menus.item' && !$skipMenu)
+		{
+			JForm::addFormPath(__DIR__ . '/params');
+			$form->loadFile('jtanimsition', true);
+		}
+		if($FB) $FB->groupEnd();
+	}
+
+	public function onBeforeRender()
+	{
+		$activeLinkId = 'item['.JFactory::getApplication()->getMenu()->getActive()->id.']';
+		$paramsSet = $this->paramsSet[$activeLinkId];
+
+		if(!JFactory::getApplication()->isSite()
+			|| empty($paramsSet)
+			|| !is_array($paramsSet)) return;
+
+		$FB = $this->FB;
+		if($FB) $FB->group('JT - Animsition -> '.__FUNCTION__);
+
+		if($FB) $FB->info($activeLinkId,'$activeLinkId');
+		if($FB) $FB->info($paramsSet,'$paramsSet');
+
+		$document = JFactory::getDocument();
+		if($FB) $FB->info($document,'$document');
+
+		$liveSite = rtrim(JURI::base(true), '/');
+		$plgName = $this->get('_name');
+		$plgType = $this->get('_type');
+		$pathToLibs = $liveSite.'/plugins/'.$plgType.'/'.$plgName.'/library';
+		$dataAnimsitionIn = $paramsSet['in'] ? ' data-animsition-in="'.$paramsSet['in']['type'].'" data-animsition-in-duration="'.$paramsSet['in']['dur'].'"' : '';
+		$dataAnimsitionOut = $paramsSet['out'] ? ' data-animsition-out="'.$paramsSet['out']['type'].'" data-animsition-out-duration="'.$paramsSet['out']['dur'].'"' : '';
+
+		if($FB) $FB->info($this,'$this');
+		if($FB) $FB->info($pathToLibs,'$pathToLibs');
+
+		if(substr($paramsSet['in']['type'], 0, 7) == 'overlay'
+			|| substr($paramsSet['out']['type'], 0, 7) == 'overlay')
+		{
+			$animOverlay = ' data-animsition-overlay="true"';
+		}
+		else {
+			$animOverlay = '';
+		}
+
+		$openAnimTag = '<div class="animsition"'.$animOverlay.$dataAnimsitionIn.$dataAnimsitionOut.'>';
+		$closeAnimTag = '</div>';
+
+		if($paramsSet['container'] == 1)
+		{
+			if(version_compare(JVERSION, '3.0', 'ge')){
+				$template = $document->getTemplateBuffer();
+			} else {
+				$template = $document->get('_template');
+			}
+
+			if($FB) $FB->info($template,'$template');
+
+			preg_match_all('#(<\s*body[^>]*>)(.*?)(<\s*/\s*body>)#siU', $template, $_template, PREG_SET_ORDER);
+			if($FB) $FB->warn($_template, '$_template');
+
+			$buffer = $_template[0][1].$openAnimTag.$_template[0][2].$closeAnimTag.$_template[0][3];
+			if($FB) $FB->warn($buffer, '$buffer');
+
+			$template = preg_replace('#(<\s*body[^>]*>)(.*?)(<\s*/\s*body>)#siU', $buffer, $template);
+
+			if(version_compare(JVERSION, '3.0', 'ge')){
+				$document->setTemplateBuffer($template);
+			} else {
+				$document->set('_template', $template);
+			}
+		}
+		else
+		{
+			$buffer = $openAnimTag.$document->getBuffer('component').$closeAnimTag;
+			if($FB) $FB->warn($buffer, '$buffer');
+			$document->setBuffer($buffer, 'component');
+		}
+
+
+		JHtml::_('jquery.framework');
+		$document->addScript($pathToLibs.'/jquery.animsition.min.js');
+		$script = 'jQuery(document).ready(function($){$(".animsition").animsition();});';
+		$document->addScriptDeclaration($script);
+		$document->addStyleSheet($pathToLibs.'/animsition.min.css');
+
+
+		if($FB) $FB->groupEnd();
+	}
+
+	protected function _setParams($global, $menu)
+	{
+		$FB = $this->FB;
+		$return = array();
+
+		if ($FB) $FB->group('-> ' . __FUNCTION__);
+
+		if (!is_array($global) || !is_array($menu))
+		{
+			if ($FB) $FB->error('$global or $menu are not an array!');
+			if ($FB) $FB->groupEnd();
+
+			return null;
+		}
+
+		foreach ($menu as $arrayKey => $arrayValue)
+		{
+			foreach ($arrayValue as $key => $value)
+			{
+				switch ($value)
+				{
+					case '-1':
+						$return[$arrayKey][$key] = ($global[$arrayKey][$key] == '0' && $key == 'weight')
+							? '' : $global[$arrayKey][$key];
+						break;
+
+					default:
+						$return[$arrayKey][$key] = $value;
+						break;
+				}
+			}
+		}
+
+		if($FB) $FB->info($return,'Nach Fertigstellung');
+		if($FB) $FB->groupEnd();
+
+		return $return;
+	}
+
+	protected function _setAnimsition($menu)
+	{
+		$FB = $this->FB;
+		if($FB) $FB->group('-> '.__FUNCTION__);
+
+		if(!is_array($menu))
+		{
+			if($FB) $FB->error('$menu is not an array!');
+			if($FB) $FB->groupEnd();
+
+			return null;
+		}
+
+		$return = $menu;
+
+		if($FB) $FB->info($return,'Vor der Bearbeitung');
+
+		foreach($return as $arrayKey=>$arrayValue)
+		{
+			if($FB) $FB->info($arrayKey,'$arrayKey');
+			if($FB) $FB->info($arrayValue,'$arrayValue');
+
+			if(!$arrayValue['active']) {
+				unset($return[$arrayKey]);
+				continue;
+			}
+
+			if(strpos($arrayValue['type'], '-') !== false)
+			{
+				if(substr($arrayValue['type'], 0, 7) == 'overlay')
+				{
+					if($FB) $FB->info('Type ist Overlay');
+
+					$return[$arrayKey]['type'] = str_replace('-', '-slide-'.$arrayKey.'-', $arrayValue['type']);
+					$arrayValue['weight'] = '';
+				}
+				else {
+					$return[$arrayKey]['type'] = str_replace('-', '-'.$arrayKey.'-', $arrayValue['type']);
+				}
+			}
+			elseif(strpos($arrayValue['type'], '-') === false) {
+				$return[$arrayKey]['type'] = $arrayValue['type'].'-'.$arrayKey;
+			}
+
+			switch($arrayValue['type'])
+			{
+				case 'flip-x':
+				case 'flip-y':
+					if($arrayValue['weight'] != '') {
+						$arrayValue['weight'] = ($arrayValue['weight'] == 'sm') ? 'fr' : 'nr';
+					}
+
+				case 'fade':
+					break;
+
+				default:
+					$return[$arrayKey]['type'] = $arrayValue['weight'] != ''
+						? $return[$arrayKey]['type'].'-'.$arrayValue['weight']
+						: $return[$arrayKey]['type'];
+					break;
+			}
+
+			unset($return[$arrayKey]['weight']);
+		}
+
+		if($FB) $FB->info($return,'Nach Fertigstellung');
+		if($FB) $FB->groupEnd();
+
+		return $return;
+	}
+}
---
+++ jtanimsition.script.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @version 1.0.1
+ * @package JT - Animsition
+ * @copyright 2014 Guido De Gobbis - JoomTools
+ * @license GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.joomtools.de
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+
+/**
+ * Class PlgSystemJTAnimsitionInstallerScript
+ */
+class PlgSystemJTAnimsitionInstallerScript
+{
+    /**
+     * Called before any type of action
+     *
+     * @param  string  $type  type of current action
+     *
+     * @return  boolean  True on success
+     */
+    public function preflight($type)
+    {
+        // make version check only when installing the plugin
+        if ($type != "discover_install" && $type != "install")
+        {
+            return true;
+        }
+
+        $version = new JVersion;
+
+        // Abort if the current Joomla release is older
+        if (version_compare($version->getShortVersion(), "3", 'lt'))
+        {
+            Jerror::raiseWarning(null, 'Cannot install JT - Animsition in a Joomla release prior to 3');
+
+            return false;
+        }
+
+        return true;
+    }
+}
