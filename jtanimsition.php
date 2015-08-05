a8d1106faf280991ab4ebde8109df684bdf26121
Guido De Gobbis <guido.de.gobbis@joomtools.de>
2015-08-05 12:42:21 +0200

    Clear some PHP-Notice / Reformat Code

--- jtanimsition.php
+++ jtanimsition.php
@@ -35,11 +35,36 @@ class plgSystemJTAnimsition extends JPlugin
             $this->FB = false;
         }
 
-        $FB           = $this->FB;
-        $globalParams = $itemParams = array();
+        $FB = $this->FB;
 
         if ($FB) $FB->group('JT - Animsition -> ' . __FUNCTION__);
         if ($FB) $FB->info($this->params, '$this->params');
+
+        $this->paramsSet['container'] = $this->params->get('anim_container', 1);
+        $this->paramsSet['positions'] = null;
+
+        if ($this->params->get('anim_container', 1) == 2)
+        {
+            $animPositions                = (array) json_decode($this->params->get('anim_pos_repeatable', ''));
+            $this->paramsSet['positions'] = (!$animPositions && !empty($animPositions['anim_position'])) ? (array) $animPositions['anim_position'] : null;
+        }
+
+        if ($FB) $FB->info($this->paramsSet['positions'], '$this->paramsSet[positions]');
+        if ($FB) $FB->groupEnd();
+    }
+
+    public function onAfterRoute()
+    {
+        if (!JFactory::getApplication()->isSite()) return;
+
+        $FB = $this->FB;
+        if ($FB) $FB->group('JT - Animsition -> ' . __FUNCTION__);
+
+        $menu       = JFactory::getApplication()->getMenu();
+        $menuItems  = $menu->getItems('access', '1');
+        $activeItem = $menu->getActive();
+
+        if ($FB) $FB->info($menu, '$menu');
 
         $globalParams['in']['active']  = $this->params->get('anim_active_in');
         $globalParams['in']['type']    = $this->params->get('anim_type_in');
@@ -51,170 +76,37 @@ class plgSystemJTAnimsition extends JPlugin
         $globalParams['out']['dur']    = $this->params->get('anim_dur_out');
         if ($FB) $FB->info($globalParams, '$globalParams');
 
-        $menuItems = JFactory::getApplication()->getMenu()->getItems('access', '1');
-        if ($FB)$FB->info($menuItems, '$menuItems');
+        $itemParams['in']['active']  = $activeItem->params->get('menu_anim_active_in', '-1');
+        $itemParams['in']['type']    = $activeItem->params->get('menu_anim_type_in', '-1');
+        $itemParams['in']['weight']  = $activeItem->params->get('menu_anim_weight_in', '-1');
+        $itemParams['in']['dur']     = $activeItem->params->get('menu_anim_dur_in', '-1');
+        $itemParams['out']['active'] = $activeItem->params->get('menu_anim_active_out', '-1');
+        $itemParams['out']['type']   = $activeItem->params->get('menu_anim_type_out', '-1');
+        $itemParams['out']['weight'] = $activeItem->params->get('menu_anim_weight_out', '-1');
+        $itemParams['out']['dur']    = $activeItem->params->get('menu_anim_dur_out', '-1');
+        if ($FB) $FB->info($itemParams, '$itemParams');
 
-        $this->paramsSet['container'] = $this->params->get('anim_container', 1);
-        $this->paramsSet['positions'] = null;
+        $setParams      = $this->_setParams($globalParams, $itemParams);
+        $setAnimsition  = $this->_setAnimsition($setParams);
 
-        if ($this->params->get('anim_container', 1) == 2)
+        if ($setParams['out']['active'])
         {
-            $animPositions                = (array) json_decode($this->params->get('anim_pos_repeatable', ''));
-            $this->paramsSet['positions'] = ($animPositions && !empty($animPositions['anim_position'])) ? (array) $animPositions['anim_position'] : null;
-        }
-
-        if ($FB) $FB->info($this->paramsSet['positions'], '$this->paramsSet[positions]');
-
-        foreach ($menuItems as &$item)
-        {
-            if ($FB) $FB->group('$item[' . $item->id . ']');
-            if ($FB) $FB->info($item, '$item');
-
-            $itemParams['in']['active']  = $item->params->get('menu_anim_active_in', '-1');
-            $itemParams['in']['type']    = $item->params->get('menu_anim_type_in', '-1');
-            $itemParams['in']['weight']  = $item->params->get('menu_anim_weight_in', '-1');
-            $itemParams['in']['dur']     = $item->params->get('menu_anim_dur_in', '-1');
-            $itemParams['out']['active'] = $item->params->get('menu_anim_active_out', '-1');
-            $itemParams['out']['type']   = $item->params->get('menu_anim_type_out', '-1');
-            $itemParams['out']['weight'] = $item->params->get('menu_anim_weight_out', '-1');
-            $itemParams['out']['dur']    = $item->params->get('menu_anim_dur_out', '-1');
-            if ($FB) $FB->info($itemParams, '$itemParams');
-
-            $setParams = $this->_setParams($globalParams, $itemParams);
-
-            if ($setParams['in']['active'] || $setParams['out']['active'])
+            foreach ($menuItems as &$item)
             {
-                if ($FB) $FB->info('item[' . $item->id . ']->active');
-
-                $setAnimsition  = $this->_setAnimsition($setParams);
                 $activLinkClass = $item->params->get('menu-anchor_css', '');
                 $activLinkClass = $activLinkClass ? $activLinkClass . ' animsition-link' : 'animsition-link';
 
                 $item->params->set('menu-anchor_css', $activLinkClass);
 
-                if ($FB) $FB->info('item[' . $item->id . ']->class->set->ok');
-
-                $this->paramsSet['item[' . $item->id . ']'] = $setAnimsition;
+                if ($FB) $FB->info('item[' . $item->id . ']->class->set->active->out->ok');
             }
-
-            if ($FB) $FB->groupEnd();
         }
 
+        $this->paramsSet['item[' . $activeItem->id . ']'] = $setAnimsition;
+
+        if ($FB) $FB->info('item[' . $activeItem->id . ']->class->set->ok');
+        if ($FB) $FB->info($menuItems, '$menuItems');
         if ($FB) $FB->groupEnd();
-    }
-
-    protected function _setParams($global, $menu)
-    {
-        $FB     = $this->FB;
-        $return = array();
-
-        if ($FB) $FB->group('-> ' . __FUNCTION__);
-
-        if (!is_array($global) || !is_array($menu))
-        {
-            if ($FB) $FB->error('$global or $menu are not an array!');
-            if ($FB) $FB->groupEnd();
-
-            return null;
-        }
-
-        foreach ($menu as $arrayKey => $arrayValue)
-        {
-            foreach ($arrayValue as $key => $value)
-            {
-                switch ($value)
-                {
-                    case '-1':
-                        $return[$arrayKey][$key] = ($global[$arrayKey][$key] == '0' && $key == 'weight')
-                            ? '' : $global[$arrayKey][$key];
-                        break;
-
-                    default:
-                        $return[$arrayKey][$key] = $value;
-                        break;
-                }
-            }
-        }
-
-        if ($FB) $FB->info($return, 'Nach Fertigstellung');
-        if ($FB) $FB->groupEnd();
-
-        return $return;
-    }
-
-    protected function _setAnimsition($menu)
-    {
-        $FB = $this->FB;
-
-        if ($FB) $FB->group('-> ' . __FUNCTION__);
-
-        if (!is_array($menu))
-        {
-            if ($FB) $FB->error('$menu is not an array!');
-            if ($FB) $FB->groupEnd();
-
-            return null;
-        }
-
-        $return = $menu;
-        if ($FB) $FB->info($return, 'Vor der Bearbeitung');
-
-        foreach ($return as $arrayKey => $arrayValue)
-        {
-            if ($FB) $FB->info($arrayKey, '$arrayKey');
-            if ($FB) $FB->info($arrayValue, '$arrayValue');
-
-            if (!$arrayValue['active'])
-            {
-                unset($return[$arrayKey]);
-                continue;
-            }
-
-            if (strpos($arrayValue['type'], '-') !== false)
-            {
-                if (substr($arrayValue['type'], 0, 7) == 'overlay')
-                {
-                    if ($FB) $FB->info('Type ist Overlay');
-
-                    $return[$arrayKey]['type'] = str_replace('-', '-slide-' . $arrayKey . '-', $arrayValue['type']);
-                    $arrayValue['weight']      = '';
-                }
-                else
-                {
-                    $return[$arrayKey]['type'] = str_replace('-', '-' . $arrayKey . '-', $arrayValue['type']);
-                }
-            }
-            elseif (strpos($arrayValue['type'], '-') === false)
-            {
-                $return[$arrayKey]['type'] = $arrayValue['type'] . '-' . $arrayKey;
-            }
-
-            switch ($arrayValue['type'])
-            {
-                case 'flip-x':
-                case 'flip-y':
-                    if ($arrayValue['weight'] != '')
-                    {
-                        $arrayValue['weight'] = ($arrayValue['weight'] == 'sm') ? 'fr' : 'nr';
-                    }
-
-                case 'fade':
-                    break;
-
-                default:
-                    $return[$arrayKey]['type'] = $arrayValue['weight'] != ''
-                        ? $return[$arrayKey]['type'] . '-' . $arrayValue['weight']
-                        : $return[$arrayKey]['type'];
-                    break;
-            }
-
-            unset($return[$arrayKey]['weight']);
-        }
-
-        if ($FB) $FB->info($return, 'Nach Fertigstellung');
-        if ($FB) $FB->groupEnd();
-
-        return $return;
     }
 
     public function onContentPrepareForm($form, $data)
@@ -257,14 +149,14 @@ class plgSystemJTAnimsition extends JPlugin
     {
         if (!JFactory::getApplication()->isSite()) return;
 
+        $FB = $this->FB;
+        if ($FB) $FB->group('JT - Animsition -> ' . __FUNCTION__);
+
         $activeLinkId = 'item[' . JFactory::getApplication()->getMenu()->getActive()->id . ']';
-        $paramsSet    = $this->paramsSet[$activeLinkId];
+        $paramsSet    = isset($this->paramsSet[$activeLinkId]) ? $this->paramsSet[$activeLinkId] : null;
 
         if (empty($paramsSet) || !is_array($paramsSet)) return;
 
-        $FB = $this->FB;
-
-        if ($FB) $FB->group('JT - Animsition -> ' . __FUNCTION__);
         if ($FB) $FB->info($activeLinkId, '$activeLinkId');
 
         $document = JFactory::getDocument();
@@ -356,17 +248,135 @@ class plgSystemJTAnimsition extends JPlugin
 		jQuery(document).ready(function($){
 		    var cssLink = $(\'<link rel="stylesheet" type="text/css" href="' . $pathToLibs . '/animsition.min.css">\');
 		    $("head").append(cssLink);
-		    $(".animsition").animsition();
+		    $(".animsition").animsition({
+		        inDuration  : 0,
+		        outDuration : 0
+		    });
         });';
         $document->addScriptDeclaration($script);
 
         if ($FB) $FB->groupEnd();
     }
 
+    protected function _setParams($global, $menu)
+    {
+        $FB     = $this->FB;
+        $return = array();
+
+        if ($FB) $FB->group('-> ' . __FUNCTION__);
+
+        if (!is_array($global) || !is_array($menu))
+        {
+            if ($FB) $FB->error('$global or $menu are not an array!');
+            if ($FB) $FB->groupEnd();
+
+            return null;
+        }
+
+        foreach ($menu as $arrayKey => $arrayValue)
+        {
+            if ($FB) $FB->info($arrayKey, '$arrayKey');
+
+            foreach ($arrayValue as $key => $value)
+            {
+                switch ($value)
+                {
+                    case '-1':
+                        $return[$arrayKey][$key] = ($global[$arrayKey][$key] == '0' && $key == 'weight') ? '' : $global[$arrayKey][$key];
+                        break;
+
+                    default:
+                        $return[$arrayKey][$key] = $value;
+                        break;
+                }
+            }
+        }
+
+        if ($FB) $FB->info($return, 'Nach Fertigstellung');
+        if ($FB) $FB->groupEnd();
+
+        return $return;
+    }
+
+    protected function _setAnimsition($menu)
+    {
+        $FB = $this->FB;
+
+        if ($FB) $FB->group('-> ' . __FUNCTION__);
+
+        if (!is_array($menu))
+        {
+            if ($FB) $FB->error('$menu is not an array!');
+            if ($FB) $FB->groupEnd();
+
+            return null;
+        }
+
+        $return = $menu;
+        if ($FB) $FB->info($return, 'Vor der Bearbeitung');
+
+        foreach ($return as $arrayKey => $arrayValue)
+        {
+            if ($FB) $FB->info($arrayKey, '$arrayKey');
+            if ($FB) $FB->info($arrayValue, '$arrayValue');
+
+            if (!$arrayValue['active'])
+            {
+                unset($return[$arrayKey]);
+                continue;
+            }
+
+            if (strpos($arrayValue['type'], '-') !== false)
+            {
+                if (substr($arrayValue['type'], 0, 7) == 'overlay')
+                {
+                    if ($FB) $FB->info('Type ist Overlay');
+
+                    $return[$arrayKey]['type'] = str_replace('-', '-slide-' . $arrayKey . '-', $arrayValue['type']);
+                    $arrayValue['weight']      = '';
+                }
+                else
+                {
+                    $return[$arrayKey]['type'] = str_replace('-', '-' . $arrayKey . '-', $arrayValue['type']);
+                }
+            }
+            elseif (strpos($arrayValue['type'], '-') === false)
+            {
+                $return[$arrayKey]['type'] = $arrayValue['type'] . '-' . $arrayKey;
+            }
+
+            switch ($arrayValue['type'])
+            {
+                case 'flip-x':
+                case 'flip-y':
+                    if ($arrayValue['weight'] != '')
+                    {
+                        $arrayValue['weight'] = ($arrayValue['weight'] == 'sm') ? 'fr' : 'nr';
+                    }
+
+                case 'fade':
+                    break;
+
+                default:
+                    $return[$arrayKey]['type'] = $arrayValue['weight'] != ''
+                        ? $return[$arrayKey]['type'] . '-' . $arrayValue['weight']
+                        : $return[$arrayKey]['type'];
+                    break;
+            }
+        }
+
+        unset($return[$arrayKey]['weight']);
+
+        if ($FB) $FB->info($return, 'Nach Fertigstellung');
+        if ($FB) $FB->groupEnd();
+
+        return $return;
+    }
+
     protected function _getAnimAttributes()
     {
         $activeLinkId = 'item[' . JFactory::getApplication()->getMenu()->getActive()->id . ']';
-        $paramsSet    = $this->paramsSet[$activeLinkId];
+        $paramsSet    = isset($this->paramsSet[$activeLinkId]) ? $this->paramsSet[$activeLinkId] : null;
 
         if (!JFactory::getApplication()->isSite() || empty($paramsSet) || !is_array($paramsSet)) return;
 
@@ -384,17 +394,21 @@ class plgSystemJTAnimsition extends JPlugin
 
         if (isset($paramsSet['in']))
         {
-            $dataAnimsitionIn = ' data-animsition-in="' . $paramsSet['in']['type'] . '" data-animsition-in-duration="' . $paramsSet['in']['dur'] . '"';
+            $dataAnimsitionIn = ' data-animsition-in="' . $paramsSet['in']['type']
+                . '" data-animsition-in-duration="' . $paramsSet['in']['dur'] . '"';
 
             $animOverlay = substr($paramsSet['in']['type'], 0, 7) == 'overlay' ? ' data-animsition-overlay="true"' : '';
         }
-        elseif (isset($paramsSet['out']))
+
+        if (isset($paramsSet['out']))
         {
-            $dataAnimsitionOut = ' data-animsition-out="' . $paramsSet['out']['type'] . '" data-animsition-out-duration="' . $paramsSet['out']['dur'] . '"';
+            $dataAnimsitionOut = ' data-animsition-out="' . $paramsSet['out']['type']
+                . '" data-animsition-out-duration="' . $paramsSet['out']['dur'] . '"';
             $animOverlay       = substr($paramsSet['out']['type'], 0, 7) == 'overlay' ? ' data-animsition-overlay="true"' : '';
         }
 
         $animAttributes = ' class="animsition"' . $animOverlay . $dataAnimsitionIn . $dataAnimsitionOut;
+        if ($FB) $FB->info($animAttributes, '$animAttributes');
 
         if ($FB) $FB->groupEnd();
 
